03ff98a95258469608ff5e1a28231d030401160c
GabrielxD123ye <52256308+GabrielxD123ye@users.noreply.github.com>
2024-06-06 10:14:07 -0300

    Update potencia.php

--- PhpProject8/potencia.php
+++ PhpProject8/potencia.php
@@ -56,7 +56,7 @@ $resultado = $conecta->query($sql);
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Potêcia</title>
+        <title>Potência</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <style>
             .navbar-inverse .navbar-nav > li > a {
