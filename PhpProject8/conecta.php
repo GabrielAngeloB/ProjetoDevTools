2474136369cfbcd87b2a5465ae8a5aea44dc9205
GabrielxD123ye <52256308+GabrielxD123ye@users.noreply.github.com>
2024-06-06 08:57:35 -0300

    Add files via upload

--- PhpProject8/conecta.php
+++ PhpProject8/conecta.php
@@ -1,12 +1,14 @@
 <?php
 
-$nome_servidor = "localhost";
-$nome_usuario = "root";
-$senhabanco = "";
-$banco = "db_projeto";
+$nome_servidor = "sql10.freesqldatabase.com";
+$nome_usuario = "sql10712214";
+$senhabanco = "FlbEkTpNzv";
+$banco = "sql10712214";
+
 $conecta = new mysqli($nome_servidor, $nome_usuario, $senhabanco, $banco);
 if ($conecta->connect_error) {
     die("Conexão falhou: " . $conecta->connect_error . "<br>");
 }
+mysqli_set_charset($conecta, "utf8mb4");
 ?>
         
